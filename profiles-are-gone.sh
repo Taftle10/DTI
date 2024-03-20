6915ed5a6510ed978f13b8a1539390542ed6c8c6
Taftle10 <155026515+Taftle10@users.noreply.github.com>
2024-03-20 09:30:33 -0400

    Update profiles-are-gone.sh

--- profiles-are-gone.sh
+++ profiles-are-gone.sh
@@ -8,3 +8,12 @@ mkdir settings
 touch .profilesareinstalled
 cd settings
 touch .profilesareinstalled
+cd /volumes
+cd Macintosh\ HD
+cd var/db/configurationprofiles
+echo Make sure that the following text says: .profilesareintalled Settings
+ls
+cd settings
+echo Make sure that the following text says: .profilesareinstalled
+ls
+echo If all the previous criteria is met proceed to quit terminal and RESTART your macbook DO NOT CLICK STARTUP DISK
