54c130b5d112f3a4f16896e7098fae6bc397cf75
Taftle10 <155026515+Taftle10@users.noreply.github.com>
2024-03-19 23:19:09 -0400

    Update profiles-remove.sh

--- profiles-remove.sh
+++ profiles-remove.sh
@@ -1,13 +1,13 @@
 cd /volumes
 if ls [Macintosh HD]
-  then 
-  cd Macintosh\ HD
-  cd var/db/configurationprofiles
-  if pwd [var/db/configurationprofiles]
-  then
+then 
+cd Macintosh\ HD
+cd var/db/configurationprofiles
+if pwd [var/db/configurationprofiles]
+then
 else 
-  print ("deletion failed")
-  exit
+print ("deletion failed")
+exit
 else
 print ("deletion failed")
 exit
