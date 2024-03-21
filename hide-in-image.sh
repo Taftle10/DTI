ae0b8e0541715ffb23df029761869570c85674af
Taftle10 <155026515+Taftle10@users.noreply.github.com>
2024-03-21 10:37:54 -0400

    Update hide-in-image.sh

--- hide-in-image.sh
+++ hide-in-image.sh
@@ -1,6 +1,6 @@
 echo "DO NOT ADD ANY SPACES TO YOUR FILES WHEN CREATING THEM"
 
-echo "What is the name of your photo image (add .png or .jpeg or whatever photo type your image is )?"
+echo "What is the name of your photo image (add .png or .jpeg or whatever photo type your image is)?"
 read PhotoImage
 
 echo "What is the name of your zip file (you can try to use other files, do the file type at the end. IE .zip)?"
