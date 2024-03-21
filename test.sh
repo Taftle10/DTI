843ceb23f4518821632d7e68bcb150fe58e668a2
Taftle10 <155026515+Taftle10@users.noreply.github.com>
2024-03-21 10:33:38 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-echo "What is the name of your photo image?"
+echo "What is the name of your photo image (add .png or .jpeg or whatever photo type your image is)?"
 read PhotoImage
 
-echo "What is the name of your zip file?"
+echo "What is the name of your zip file (you can try to use other files, do the file type at the end. IE .zip)?"
 read ZipFile
 
-echo "What do you want your new file to be called?"
+echo "What do you want your new file to be called (add .png or .jped or whatever photo type you want)?"
 read NewFile
+
+cd Desktop
+cat $PhotoImage $ZipFile > $NewFile
